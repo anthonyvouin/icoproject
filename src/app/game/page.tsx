1edaaa531145fe62977e99e9c6ba07f9b7fac418
Berki Khalida <khalidaberki1@gmail.com>
2025-01-18 15:42:22 +0100

    Supprimer l'effet hover du bouton handleShowPopin

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -165,8 +165,8 @@ export default function Game() {
           Êtes-vous bien ${gameState.players[playerId].name} ?
         </p>
         <div class="flex justify-center gap-4">
-          <button id="confirmYes" class="bg-[#7D4E1D] text-white px-4 py-2 rounded hover:bg-blue-700">Oui</button>
-          <button id="confirmNo" class="bg-[#383837] text-white px-4 py-2 rounded hover:bg-gray-400">Non</button>
+          <button id="confirmYes" class="bg-[#7D4E1D] text-white px-4 py-2 rounded ">Oui</button>
+          <button id="confirmNo" class="bg-[#383837] text-white px-4 py-2 rounded ">Non</button>
         </div>
       </div>
     `;
